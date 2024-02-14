14f82662dfeb121fb38c9e2ed7c43d74cd82616a
jiylee <jjenny4711@gmail.com>
2024-02-14 02:39:05 -0500

    3,4

--- main.js
+++ main.js
@@ -1,24 +1,64 @@
 const input=document.getElementById('input')
 const addBtn=document.getElementById('addBtn')
 const taskBoard=document.getElementById('taskBoard')
+const checkBtn=document.getElementById('checkBtn')
+const deleteBtn=document.getElementById('deleteBtn')
+const plan=document.getElementById('plan')
 let todoList=[]
+function randomId() {
+  return "-" + Math.random().toString(36);
+}
+
 addBtn.addEventListener("click",addTask)
+
 function addTask(){
-  let taskContent = input.value
-  todoList.push(taskContent)
-  console.log(todoList,'taskList')
+  
+  let task={
+    id:randomId(),
+    taskContent:input.value,
+    isComplete:false,
+  }
+  todoList.push(task)
+  input.value =""
+
   render()
+}
+
+function handleDelete (id){
+for(let i=0;i<todoList.length; i++){
+  if(todoList[i].id === id){
+    todoList.splice(i,1);
+    break;
+  }
+}
+render()
+}
+
+function handleCheck (id){
+  console.log(id,'id')
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].id == id) {
+      todoList[i].isComplete = !todoList[i].isComplete;
+      break;
+    }
+  }
+  render();
+  console.log(todoList);
+
 }
 
 function render(){
   let resultHTML ='';
   for(let i=0;i<todoList.length;i++){
+ 
+ const taskClass=todoList[i].isComplete?'taskDone':''
+  
     resultHTML +=`<div class="task">
-    <div>${todoList[i]}</div>
+    <div class='${taskClass}'>${todoList[i].taskContent}</div>
 
     <div>
-      <button>check</button>
-      <button>delete</button>
+      <button id='checkBtn' onclick="handleCheck('${todoList[i].id}')">check</button>
+      <button id='deleteBtn' onclick="handleDelete('${todoList[i].id}')">delete</button>
     </div>
 
    </div>`
