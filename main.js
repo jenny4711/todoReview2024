54c0a2cc95a9334819d5865a98b12f12b189d3ee
jiylee <jjenny4711@gmail.com>
2024-02-15 04:11:06 -0500

    5

--- main.js
+++ main.js
@@ -4,12 +4,32 @@ const taskBoard=document.getElementById('taskBoard')
 const checkBtn=document.getElementById('checkBtn')
 const deleteBtn=document.getElementById('deleteBtn')
 const plan=document.getElementById('plan')
+let taps=document.querySelectorAll(".taskTaps div")
+let underLine = document.getElementById("underLine");
+let mode="all"
+let filterList=[]
 let todoList=[]
+for(let i=1;i<taps.length;i++){
+  taps[i].addEventListener("click",function(evt){filter(evt)})
+  taps[i].addEventListener("click", (e) => moveUnderLine(e));
+}
+
+
+
+
 function randomId() {
   return "-" + Math.random().toString(36);
 }
 
 addBtn.addEventListener("click",addTask)
+
+
+function moveUnderLine(evt){
+  underLine.style.left = evt.currentTarget.offsetLeft + "px";
+  underLine.style.width = evt.currentTarget.offsetWidth + "px";
+  underLine.style.top =
+    evt.currentTarget.offsetTop + evt.currentTarget.offsetHeight + "px";
+}
 
 function addTask(){
   
@@ -22,6 +42,31 @@ function addTask(){
   input.value =""
 
   render()
+}
+
+
+
+function filter(evt) {
+  mode = evt.target.id;
+  filterList = [];
+  if (mode === "all") {
+    render();
+  } else if (mode === "ongoing") {
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i].isComplete == false) {
+        filterList.push(todoList[i]);
+      }
+    }
+
+    render();
+  } else if (mode === "done") {
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i].isComplete === true) {
+        filterList.push(todoList[i]);
+      }
+    }
+  }
+  render();
 }
 
 function handleDelete (id){
@@ -48,17 +93,27 @@ function handleCheck (id){
 }
 
 function render(){
+  let list =[]
+if(mode ==="all"){
+list =todoList
+}else if(mode === "ongoing"){
+list = filterList
+}else if(mode === "done"){
+  list =filterList
+}
+
+
   let resultHTML ='';
-  for(let i=0;i<todoList.length;i++){
+  for(let i=0;i<list.length;i++){
  
- const taskClass=todoList[i].isComplete?'taskDone':''
+ const taskClass=list[i].isComplete?'taskDone':''
   
     resultHTML +=`<div class="task">
-    <div class='${taskClass}'>${todoList[i].taskContent}</div>
+    <div class='${taskClass}'>${list[i].taskContent}</div>
 
     <div>
-      <button id='checkBtn' onclick="handleCheck('${todoList[i].id}')">check</button>
-      <button id='deleteBtn' onclick="handleDelete('${todoList[i].id}')">delete</button>
+      <button id='checkBtn' onclick="handleCheck('${list[i].id}')">check</button>
+      <button id='deleteBtn' onclick="handleDelete('${list[i].id}')">delete</button>
     </div>
 
    </div>`
